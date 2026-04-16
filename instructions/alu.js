83a132efb1c5e0963d11a3435976bce9cfcb9345
corey-h-txst <lzs8@txstate.edu>
2026-04-16 18:54:54 -0500

    Further implemented alu.js as test for quiz/learn system

--- instructions/alu.js
+++ instructions/alu.js
@@ -2,7 +2,7 @@ export const aluInstruction = {
     id: 'alu',
     label: 'ALU Instruction',
     steps: [
-        // Step 1
+        // Step 1 - PC
         {
             componentId: 'pc',
             tour: {
@@ -15,7 +15,7 @@ export const aluInstruction = {
                 answer: 0,
             }
         },
-        // Step 2
+        // Step 2 - Instruction Memory
         {
             componentId: 'instruction-mem',
             tour: {
@@ -28,6 +28,174 @@ export const aluInstruction = {
                 answer: 0,
             }
         },
-        // Future steps go here
+        // Step 3 - Control
+        {
+            componentId: 'control',
+            tour: {
+                title: 'Control',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 4 - MUX Reg-Dst
+        {
+            componentId: 'mux-reg-dst',
+            tour: {
+                title: 'Multiplexer (MUX) - Register Destination',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 5 - Register File
+        {
+            componentId: 'reg-file',
+            tour: {
+                title: 'Register File',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 6 - Sign Extend
+        {
+            componentId: 'sign-ext',
+            tour: {
+                title: 'Sign Extend',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 7 - MUX ALU-Src
+        {
+            componentId: 'mux-alu-src',
+            tour: {
+                title: 'Multiplexer (MUX) - ALU Source',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 8 - ALU Control
+        {
+            componentId: 'alu-control',
+            tour: {
+                title: 'ALU Control',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 9 - ALU
+        {
+            componentId: 'alu',
+            tour: {
+                title: 'Arithmetic Logic Unit (ALU)',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 10 - Data Memory
+        {
+            componentId: 'data-mem',
+            tour: {
+                title: 'Data Memory',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 11 - MUX Mem-To-Reg
+        {
+            componentId: 'mux-mem-to-reg',
+            tour: {
+                title: 'Multiplexer (MUX) - Memory to Register',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 12 - Register File
+        {
+            componentId: 'reg-file',
+            tour: {
+                title: 'Register File',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 13 - Adder PC
+        {
+            componentId: 'adder-pc',
+            tour: {
+                title: 'Add',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 14 - MUX PC-Src
+        {
+            componentId: 'mux-pc-src',
+            tour: {
+                title: 'Multiplexer (MUX) - PC Source',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
+        // Step 15 - PC
+        {
+            componentId: 'pc',
+            tour: {
+                title: 'Program Counter (PC)',
+                body: 'Placeholder Educational Text'
+            },
+            quiz: {
+                question: 'Placeholder Question?',
+                body: ['Answer 1', 'Answer 2', 'Answer 3', 'Answer 4'],
+                answer: 0,
+            }
+        },
     ],
 };
